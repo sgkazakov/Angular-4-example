8c38e73b6ef59e8cafc7c1b68eb3df20f86a9969
Mihail Madzharov <mihail.madzharov.m@gmail.com>
2017-10-22 17:23:25 +0300

    content and view child example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header.component';
+import { ViewChildComponent } from './components/view-content-child/view-child/view-child.component';
+import { ContentChildComponent } from './components/view-content-child/content-child/content-child.component';
+import { CombineChildComponent } from './components/view-content-child/combine-child/combine-child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ViewChildComponent,
+    ContentChildComponent,
+    CombineChildComponent
   ],
   imports: [
     BrowserModule
