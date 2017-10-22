548ad059472a5143710e4de062d4a0a93f7913a2
Mihail Madzharov <mihail.madzharov.m@gmail.com>
2017-10-22 17:55:01 +0300

    ViewChildren added

--- src/app/components/view-content-child/combine-child/combine-child.component.ts
+++ src/app/components/view-content-child/combine-child/combine-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, ViewChild, AfterContentInit, AfterViewInit, ElementRef } from "@angular/core";
+import { Component, ContentChild, ViewChild, AfterContentInit, AfterViewInit, ElementRef, ViewChildren, QueryList } from "@angular/core";
 import { ContentChildComponent } from "../content-child/content-child.component";
 import { ViewChildComponent } from "../view-child/view-child.component";
 
@@ -11,20 +11,29 @@ export class CombineChildComponent implements AfterContentInit, AfterViewInit {
     @ViewChild(ViewChildComponent)
     private viewChild: ViewChildComponent;
 
+    @ViewChildren(ContentChildComponent)
+    private divChildren: QueryList<ContentChildComponent>;
+
     private changdViewChildColor() {
         this.viewChild.viewChildColor = !this.viewChild.viewChildColor;
     }
 
     public ngAfterContentInit(): void {
         debugger
+        if (this.divChildren != null) {
+
+            this.divChildren.forEach(a => console.log(a.text))
+        }
         // this div doesn't exist yet
         // this.viewChild.viewChildColor = true;
     }
 
     public ngAfterViewInit(): void {
         debugger
+        
         // you can't change the value in after view init
         // you can if you call change detection
+        // this.divChildren.forEach((a, index) => a.text = index.toString())
         // this.viewChild.viewChildColor = false;
     }
 }
