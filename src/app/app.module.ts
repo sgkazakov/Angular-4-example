32f2400115cbc72659d6f558f7f56d144405843a
sgk <sgkazakov@outlook.com>
2017-10-22 18:25:18 +0300

    Adding pipes examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header.component';
+import { CustomPipePipe } from './pipes/custom-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CustomPipePipe
   ],
   imports: [
     BrowserModule
--- src/app/components/header.component.ts
+++ src/app/components/header.component.ts
@@ -1,12 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, AfterContentInit, AfterViewInit, AfterContentChecked } from "@angular/core";
+import { TitleCasePipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
+import { CustomPipePipe } from '../pipes/custom-pipe.pipe';
 
 @Component({
-    moduleId: module.id,
+
     selector: "header-component",
     templateUrl: "./header.component.html",
-    styleUrls: ["./header.component.css"]
-}
-)
-export class HeaderComponent {
+    styleUrls: ["./header.component.css"],
+    providers: [
+        DatePipe,
+    ]
+})
+export class HeaderComponent implements AfterContentInit {
+
+    private title = "Angular Recipies";
+    private time: string;
+
+    constructor(private datePipe: DatePipe) {
+    }
+
+    ngAfterContentInit() {
+        this.time = this.datePipe.transform(new Date(), "yyyy/MMM/dd");
+    }
 
 }
